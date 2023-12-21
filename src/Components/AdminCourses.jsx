d58ccb610ed4c73561cabfabfeccc975943b3e2f
pablomos <mosconpablo@gmail.com>
2023-12-21 17:24:00 -0300

    commit 18 44

--- src/Components/AdminCourses.jsx
+++ src/Components/AdminCourses.jsx
@@ -30,8 +30,8 @@ const AdminCourses = () => {
     
 <Container className="mt-5 mb-5">
       <Row>
-        <Col className="d-flex flex-column justify-content-center align-content-center">
-          <Table bordered className="text-center">
+        <Col className="mx-auto">
+          <Table bordered className="text-center mx-auto">
             <tbody className="custom-font-size-course-table align-content-center">
               <tr className="fw-bold">
                 <td className="align-middle">Título</td>
