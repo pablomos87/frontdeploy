d8e1d967627864f38a22235620bc0e6bf3fced2b
pablomos <mosconpablo@gmail.com>
2023-12-21 17:51:35 -0300

    commit 18 45

--- src/Components/AdminCourseRegistration.jsx
+++ src/Components/AdminCourseRegistration.jsx
@@ -65,7 +65,7 @@ const AdminCourseRegistration = () => {
     <Container className="mx-auto mt-5 mb-5">
       <Row>
         <Col>
-          <Table bordered className="mx-auto w-75 text-center">
+          <Table bordered className="mx-auto w-75 text-center custom-font-size-registration-table">
             <tbody>
               <tr className="fw-bold custom-font-size-registration-table align-content-center">
                 <td className="align-middle">Usuario</td>
@@ -74,7 +74,7 @@ const AdminCourseRegistration = () => {
               </tr>
               {users.map((user) => (
                 user.registeredCourses.map((course, index) => (
-                  <tr key={`${user._id}-${course._id}`}>
+                  <tr key={`${user._id}-${course._id}`} className="custom-font-size-registration-table">
                     {index === 0 && (
                       <td className="custom-font-size-registration-table" rowSpan={user.registeredCourses.length}>{user.username}</td>
                     )}
